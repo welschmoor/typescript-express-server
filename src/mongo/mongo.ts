422df6ff02890d52098ce636a719e3679eabb0c7
Johannes Welsch <welschmoor@gmail.com>
2022-03-15 21:32:28 +0100

    generics added

--- src/mongo/mongo.ts
+++ src/mongo/mongo.ts
@@ -1,16 +1,18 @@
 import dotenv from "dotenv"
 dotenv.config()
 
+import { Database } from "../types/types";
+
 import { MongoClient } from "mongodb";
 
-const MONGO_URI = 'mongodb+srv://hunni:<password>@cluster0.ivs3r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MONGO_URI = 'mongodb+srv://hunni:hurensohn@cluster0.ivs3r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-export const connectMongo = async () => {
+export const connectMongo = async (): Promise<Database> => {
   const client = await MongoClient.connect(MONGO_URI)
 
   const DB = client.db('DB')
 
-  return {
+  return {   
     listings: DB.collection('test')
   }
 }
---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+import { Collection, ObjectId } from "mongodb";
+
+// _id is a mongo thing
+export interface Listing {
+  _id: ObjectId;
+  title: string;
+  image: string;
+  price: number;
+}
+
+export interface Database {
+  listings: Collection<Listing>;
+
+}
+
+ 
